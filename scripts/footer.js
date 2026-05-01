07c0b8f26cfa7327fc7e4ab62cb82ccf0346c187
harrymkt <harrymkt@users.noreply.github.com>
2026-05-01 07:11:58 +0000

    deploy: 2861f094d010d87f6c3d0e9b6c51ba40172aab84

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,15 +1,16 @@
 // Footer.
+const birthyear = 2024;
 function addfooter() {
 	// Create a footer element
 	var footerElement = document.createElement("footer");
 	// Create a paragraph element
 	var copyrightParagraph = document.createElement("p");
 	// Create a copyright text node
-	var copyrightText = document.createTextNode("Copyright \u00A9 2024–" + new Date().getFullYear() + ', ');
+	var copyrightText = document.createTextNode("Copyright \u00A9 " + birthyear + "–" + new Date().getFullYear() + ', ');
 	// Create an anchor element
 	var anchorElement = document.createElement("a");
 	anchorElement.href = "https://harrymkt.github.io";
-	anchorElement.textContent = "Harry Min Khant.";
+	anchorElement.textContent = "Harry Min Khant";
 	// Append the copyright text and anchor element to the paragraph
 	copyrightParagraph.appendChild(copyrightText);
 	copyrightParagraph.appendChild(anchorElement);
